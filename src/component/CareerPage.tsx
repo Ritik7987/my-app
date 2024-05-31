11ac100f65aa84d398613b48b609f3391573f92d
Gunjan Jain <jain10gunjan@gmail.com>
2024-05-31 15:24:33 +0530

    Added Animations

--- src/component/CareerPage.tsx
+++ src/component/CareerPage.tsx
@@ -9,6 +9,7 @@ import Placements from './career-page/Placements';
 import JobOpening from './career-page/Openings';
 import Lifeat from './career-page/Lifeat';
 import Header2 from './career-page/c2/Header';
+import HowTo from './career-page/HowTo';
 const CareerPage = () => {
   return (
     <>
@@ -16,6 +17,7 @@ const CareerPage = () => {
             <Placements/>
             <JobOpening/>
             <Lifeat/>
+            <HowTo/>
     </>
   )
 }
